a5b34f84f6882b6b82620ca21e0eb08fbaabd85c
bezabu <>
2023-10-02 13:38:04 +0000

    fill canvas with smaller squares to check if any is being cut off

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,7 @@ let CanvasWidth = canvas.width = 800;
 let CanvasHeight = canvas.height = 450;
 
 ctx.fillRect(0, 0, CanvasWidth, CanvasHeight);
+ctx.fillStyle = "#ffffff";
+ctx.fillRect(CanvasWidth * 0.1, CanvasHeight * 0.1, CanvasWidth * 0.8, CanvasHeight * 0.8);
+ctx.fillStyle = "#000000";
+ctx.fillRect(CanvasWidth * 0.2, CanvasHeight * 0.2, CanvasWidth * 0.6, CanvasHeight * 0.6);
