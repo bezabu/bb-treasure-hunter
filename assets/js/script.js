5e5116edf94a8259d52056b906d6fdcaa3afdf60
bezabu <>
2023-10-02 13:34:00 +0000

    fill canvas with black to test sizing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,5 @@ let canvas = document.getElementById("game-area");
 let ctx = canvas.getContext("2d");
 let CanvasWidth = canvas.width = 800;
 let CanvasHeight = canvas.height = 450;
+
+ctx.fillRect(0, 0, CanvasWidth, CanvasHeight);
